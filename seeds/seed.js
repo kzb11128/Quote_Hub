1ba87df42410b0faaeae8255a953448a236ddb8c
bcoleman56 <brettcoleman369@gmail.com>
2023-07-13 19:58:52 -0400

    Updated seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -10,7 +10,10 @@ const userSeedData = require('./userSeedData.json');
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-
     const users = await User.bulkCreate(userSeedData);
+    const quotes = await Quote.bulkCreate(quoteSeedData);
 
-}
+    process.exit(0);
+};
+
+seedDatabase();
