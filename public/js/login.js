893e31ac551fb5ad709737c0256d8bc6d3e8bb79
Zhang <kzb11128@gmail.com>
2023-07-18 13:38:10 -0400

    added login and logout client-side codes

---
+++ public/js/login.js
@@ -0,0 +1,56 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  //collect values from login form
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+
+  if(email && password) {
+      //send a POST request to the API endpoint
+      const response = await fetch("/", {
+          method: "POST",
+          body: JSON.stringify({ email, password}),
+          headers: { "Content-Type": "application/json"},
+      });
+      console.log(response);
+      if(response.ok){
+          document.location.replace("/profile");
+      }else{
+          alert(response.statusText);
+      }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if(username && email && password) {
+      const response = await fetch("api/user", {
+          method: "POST",
+          body: JSON.stringify({ username, email, password}),
+          headers: { "Content-Type": "application/json"},
+      });
+
+      if(response.ok){
+          document.location.replace("/profile");
+      }else{
+          alert(response.statusText);
+      }
+  }
+}
+
+const loginForm = document.querySelector(".login-form");
+const signUpForm = document.querySelector(".signup-form");
+
+if(loginForm){
+  loginForm.addEventListener("submit", loginFormHandler);
+}
+if(signUpForm){
+  signUpForm.addEventListener("submit", signupFormHandler);
+}
+
