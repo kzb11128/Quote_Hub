eb57bb586e13bc26c1cdc681507cf52b1f2b3038
Zhang <kzb11128@gmail.com>
2023-07-20 22:16:37 -0400

    fixed more bugs

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,11 +4,13 @@ const { User } = require('../../models');
 //create new user
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    console.log(req.body);
+    const userData = await User.create(req.body);
+    // const userData = await User.create({
+    //   name: req.body.name,
+    //   email: req.body.email,
+    //   password: req.body.password,
+    // });
 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -17,7 +19,8 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err);
+    res.status(400).json(JSON.stringify(err));
   }
 });
 
--- public/js/login.js
+++ public/js/login.js
@@ -26,22 +26,23 @@ const loginFormHandler = async (event) => {
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
+  console.log("check signup");
 
-  const username = document.querySelector('#username-signup').value.trim();
+  const name = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if(username && email && password) {
-      const response = await fetch("api/user", {
+  if(name && email && password) {
+      const response = await fetch("/api/users", {
           method: "POST",
-          body: JSON.stringify({ username, email, password}),
+          body: JSON.stringify({ name, email, password}),
           headers: { "Content-Type": "application/json"},
       });
 
       if(response.ok){
           document.location.replace("/profile");
       }else{
-          alert(response.statusText);
+          alert(JSON.parse(response.body));
       }
   }
 }
